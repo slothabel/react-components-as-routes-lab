811171b3c022dff0ac3bdda38ffae58ce3d3744a
Yoshi <yoshimejia00@gmail.com>
2021-11-04 13:11:47 -0400

    commit tree bd2cbabbf0ddad1236e9d0b0fad3646a18156488

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => 
+        <div key={index}>
+          <h2>Actor: {actor.name}</h2>
+          <h2>Movies:
+            <ul>
+              {actor.movies.map((movie, index) => 
+                <li key={index}>{movie}</li>
+              )}
+            </ul>
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
